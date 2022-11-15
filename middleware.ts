69bf8c5320578a4255b2c6912a6333b029c36cc4
Real-Bird <marbil815@gmail.com>
2022-11-16 00:32:05 +0900

    22-11-16 vercel analytics

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const authorization = req.cookies.get("myGroundSession");
-  if (!authorization && !req.nextUrl.pathname.includes("/contact")) {
-    if (
-      req.nextUrl.pathname.includes("/upload") ||
-      req.nextUrl.pathname.includes("/revised")
-    ) {
-      if (req.nextUrl.pathname.includes("/portfolio")) {
-        return NextResponse.redirect(new URL("/portfolio", req.url));
-      }
-      if (req.nextUrl.pathname.includes("/blog")) {
-        return NextResponse.redirect(new URL("/blog", req.url));
-      }
+  if (
+    !authorization &&
+    !req.nextUrl.pathname.includes("/contact") &&
+    (req.nextUrl.pathname.includes("/upload") ||
+      req.nextUrl.pathname.includes("/revised"))
+  ) {
+    if (req.nextUrl.pathname.includes("/portfolio")) {
+      return NextResponse.redirect(new URL("/portfolio", req.url));
+    }
+    if (req.nextUrl.pathname.includes("/blog")) {
+      return NextResponse.redirect(new URL("/blog", req.url));
     }
   }
 }
