55faac06cee1902bc59574e2e2924052f8706839
Real-Bird <marbil815@gmail.com>
2023-05-22 23:28:48 +0900

    fix: remove resume
    23-05-22

--- components/home/IntroduceCmpt.tsx
+++ components/home/IntroduceCmpt.tsx
@@ -30,27 +30,6 @@ export const Introduce = ({ ok }: IntroduceProps) => {
                 className="h-40 w-32 bg-slate-500"
                 alt="portrait"
               />
-              <Link href="/resume">
-                <a target={"_blank"}>
-                  <div className="flex flex-row">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z"
-                      />
-                    </svg>
-                    <span>이력서 보기</span>
-                  </div>
-                </a>
-              </Link>
               <div className="flex h-10 w-40 flex-row justify-evenly">
                 <Link href="https://github.com/real-bird">
                   <a className="h-8 w-8 cursor-pointer" target="_blank">
--- notice/02-refactoring-plan.md
+++ notice/02-refactoring-plan.md
@@ -1,7 +1,7 @@
 ---
 title: Refactoring Plan
 createdAt: 2023.04.17
-updatedAt: 2023.04.23
+updatedAt: 2023.05.21
 category: sub
 ---
 
@@ -14,6 +14,7 @@ category: sub
 - [x] `SSG` 전환 - **23.04.19 수정**
 - [x] `page`, `container`, `component` 분리 **23.04.20 수정**
 - [x] 태그 기능 수정(여러 태그 추가/수정/삭제) **23.04.21~23.04.23 수정**
+- [x] `SEO` 상향을 위한 `meta` 태그 추가 **23.05.20~23.05.21**
 - [ ] `upload/revised` UI 변경 및 코드 수정
 - [ ] 키워드 별 필터 설정
 
@@ -26,7 +27,9 @@ category: sub
 ### 3. notice
 
 - [ ] UI 변경
+  > 참고
+  > ![참고](https://cms.pixso.net/images/kr/articles/2022/skills/what-is-bulletin-board-design1.png)
 
 ### 4. home
 
-- [ ] 스택에 `mouseover`하면 현재 수준 팝업
+- [ ] 이력서 스타일로 UI 변경(resume 탭, self-introduction 탭)
