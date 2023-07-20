01888373e3958916c3ee38b42303af14f640594d
Real-Bird <marbil815@gmail.com>
2023-07-20 22:50:35 +0900

    fix: add cache-control in service worker
    23-07-20

--- components/common/MobileNavbarCmpt.tsx
+++ components/common/MobileNavbarCmpt.tsx
@@ -18,20 +18,20 @@ export const MobileNavigation = ({ pathname }: MobileNavigationProps) => {
           )}
         >
           <svg
+            xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
             fill="none"
-            stroke="currentColor"
             viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+            stroke="currentColor"
+            strokeWidth={2}
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-            ></path>
+              d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+            />
           </svg>
-          <span>홈</span>
+          <span>블로그</span>
         </a>
       </Link>
       <Link href="/portfolio">
@@ -58,32 +58,6 @@ export const MobileNavigation = ({ pathname }: MobileNavigationProps) => {
             ></path>
           </svg>
           <span>포트폴리오</span>
-        </a>
-      </Link>
-      <Link href="/blog">
-        <a
-          className={cls(
-            pathname == "/blog"
-              ? "text-amber-600"
-              : "text-gray-500 transition-colors dark:text-white",
-            "flex flex-col items-center space-y-2"
-          )}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
-            />
-          </svg>
-          <span>블로그</span>
         </a>
       </Link>
       <Link href="/contact">
